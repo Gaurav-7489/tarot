50d9fb46f99b4641211553a3c4c7e145dbb0b4cb
Gaurav <gauravbhardwaj7489@gmail.com>
2026-09-10 09:23:20 +0530

    perf: use local tarot image assets

--- src/app/(site)/images.ts
+++ src/app/(site)/images.ts
@@ -1,5 +1,5 @@
-// Dark tarot imagery used as the visual direction for REV.
-export const ritualImage = 'https://soykairos.com/blog/que-es-una-lectura-de-tarot_1780560749320.png'
-export const goatImage = 'https://i.etsystatic.com/6852575/r/il/e21da5/7813484875/il_fullxfull.7813484875_jhhw.jpg'
-export const cardsImage = 'https://i.pinimg.com/originals/4a/dc/56/4adc56e56a5422ad2a519560429bd96d.jpg'
-export const tableImage = 'https://static.wixstatic.com/media/f599e7_18d2eeb78c9244469f7a5eec92b8ca81~mv2.png/v1/fill/w_980%2Ch_980%2Cal_c%2Cq_90%2Cusm_0.66_1.00_0.01%2Cenc_avif%2Cquality_auto/f599e7_18d2eeb78c9244469f7a5eec92b8ca81~mv2.png'
+// Local assets uploaded to /public for fast, predictable delivery.
+export const ritualImage = '/ritual-room.jpeg'
+export const goatImage = '/demon-mask.jpeg'
+export const cardsImage = '/tarot-cards.jpeg'
+export const tableImage = '/tarot-table.jpeg'
